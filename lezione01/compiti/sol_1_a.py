4bfe2de0caf0ccbdf4b76729e79cfba69ff50613
Bob2001AC <ottaviolilliu123@gmail.com>
2022-09-12 22:09:48 +0200

    Completamento esercizio 1
    si chieda all'utente: nome, cognome, ed età - FATTO
    - si verifichi che il nome e il cognome siano più lunghi di un carattere e che l'età sia maggiore o uguale di 18 anni- FATTO
    - se tutto è corretto stampare una string di conferma altrimenti comunicare l'errore all'utente- FATTO
    - si consideri che l'utente fornirà sempre dati di tipo corretto

--- lezione01/compiti/sol_1_a.py
+++ lezione01/compiti/sol_1_a.py
@@ -4,13 +4,23 @@ from mailbox import NotEmptyError
 
 a = "Benvenuti al casinò di Ottavio"
 b = "Per giocare creare un account"
-c = "Iscriviti"
+c = "Inserisci i dati richiesti"
 print(a)
 print(b)
 print(c)
 
-d = input("Nome:")
-e = input("Cognome:")
-f = input("Età:")
+d = input ("Nome:  ")
+e = input ("Cognome: ")
+f = input ("Età:")
 
 print(input)
+
+if (len(d)) > 2: 
+ print("Accettato")
+else:
+ print("Paramentro non corretto")
+
+if (len(e)) > 2:
+    print("Accettato")
+else:
+    print(type(e) and ("Paramentro non corretto"))    
---
+++ lezione01/compiti/soluzione_1_a.py
@@ -0,0 +1,16 @@
+print("Benvenuti al casinò da Bob")
+print("                          ")
+print("Per giocare è neccessario un account")
+print("                                    ")
+print("Inserisci i dati richiesti")
+print("                          ")
+
+a = input("Nome: ")
+b = input("Cognome: ")
+c = input("Età: ")
+print(input)
+
+if (len(a)) and (len(b)) > 1 and int(c) > 18:
+ print("I dati inseriti sono validi")
+else: 
+ print("Inserire nuovamente i dati: ")
