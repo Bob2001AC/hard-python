2deb1b2cd292890ce3f83e343ad05d44b374e02a
Bob2001AC <ottaviolilliu123@gmail.com>
2022-09-12 20:11:20 +0200

    Create sol_1_a.py
    Creazione struttura logica per la pagina di iscrizione al Casinò

---
+++ lezione01/compiti/sol_1_a.py
@@ -0,0 +1,16 @@
+from importlib import import_module
+from mailbox import NotEmptyError
+
+
+a = "Benvenuti al casinò di Ottavio"
+b = "Per giocare creare un account"
+c = "Iscriviti"
+print(a)
+print(b)
+print(c)
+
+d = input("Nome:")
+e = input("Cognome:")
+f = input("Età:")
+
+print(input)
