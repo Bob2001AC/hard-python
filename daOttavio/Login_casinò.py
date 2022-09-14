68b6df2b1df7a269d54db1c17dd649ae442bec6f
Bob2001AC <ottaviolilliu123@gmail.com>
2022-09-14 22:05:52 +0200

    Creazione della pagina "Login" del casinò
    1) stampa le frasi di benvenuto e le indicazioni per iscriversi
    2) Ti fa scegliere tra 2 lingue:
    - Italiano
    - Inglese
    3)  STAMPARE IL RESOCONTO DEI DATI INSERITI + LA LINGUA SELEZIONATA
    4) ESEGUIRE IL CONTROLLO DEI DATI INSERITI E STAMPARE I'EVENTUALE ISCRIZIONE EFFETTUATA CON SUCESSO OPPURE NO.

---
+++ daOttavio/Login_casinò.py
@@ -0,0 +1,21 @@
+print("- - - Welcome to the casinò - - -\n")
+print(" Enter the required data to create your account \n")
+print("Select language:     \n")
+
+language = input("Language:  ")
+
+a = ("Italian")
+b = ("English")
+
+if ("Italian") == language :
+ input("Nome:   ")
+ input("Cognome:  ")
+ input("Età:   ")
+ 
+
+else: ("English") == language 
+input("Name:   ")
+input("Surname:   ")
+input("Ege:   ")
+
+ 
