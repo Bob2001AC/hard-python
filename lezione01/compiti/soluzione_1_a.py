462c3a5e4b8ab1b596c2ccec5be6b7e285b8b90b
Bob2001AC <ottaviolilliu123@gmail.com>
2022-09-14 13:44:04 +0200

    Guarda Claudio

--- lezione01/compiti/soluzione_1_a.py
+++ lezione01/compiti/soluzione_1_a.py
@@ -1,4 +1,4 @@
-from tkinter import N
+import string
 
 
 print("Benvenuti al casinò da Bob")
@@ -8,12 +8,26 @@ print("Per giocare è neccessario un account")
 print("Inserisci i dati richiesti")
 "\n"
 
-a = input("Nome: ")
-b = input("Cognome: ")
-c = input("Età: ")
-print(input)
-
-if (len(a)) and (len(b)) > 1 and int(c) >= 18:
+nome = input("Nome: ")
+cognome = input("Cognome: ")
+eta = input("Età: ")
+if (len(nome)) and (len(cognome)) > 1 and int(eta) >= 18:
  print("I dati inseriti sono validi")
+ print("Il nome inserito è: " + nome)
+ print("Il cognome inserito è: " + cognome)
+ print("L'età inserita è: " + eta)
 else: 
  print("Inserire nuovamente i dati: ")
+#while len(nome) <= 1 or len(cognome) <= 1 or eta <= 1:
+ print("I dati inseriti sono validi")
+ print("Il nome inserito è: " + nome)
+ print("Il cognome inserito è: " + cognome)
+ print("L'età inserita è: " + eta)
+
+string = nome
+nome= string.capitalize()
+print(nome)
+string = cognome
+cognome= string.capitalize()
+print(cognome)
+ 
