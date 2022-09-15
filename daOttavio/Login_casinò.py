a2eb84b5873b2c7e8f5935d1860c3b75504b6c81
Bob2001AC <ottaviolilliu123@gmail.com>
2022-09-15 23:14:13 +0200

    Sviluppo.

--- daOttavio/Login_casinò.py
+++ daOttavio/Login_casinò.py
@@ -1,21 +1,35 @@
+print("\n")
 print("- - - Welcome to the casinò - - -\n")
-print(" Enter the required data to create your account \n")
-print("Select language:     \n")
+print(" - - -Enter the required data to create your account - - -\n")
+list_language=["Italian","English"]
+#LanguageOne = "Italian"
+#LanguageTwo = "English"
+LanguageSelect= input("Select LAnguage:  ")
 
-language = input("Language:  ")
+while list_language[0] or list_language[1] :
+    print("The language select is  :"+LanguageSelect)
+#if LanguageSelect == len(list_language[0] or list_language[1]):#LanguageOne and LanguageTwo :
+    #print("The language select is:  " + LanguageSelect)
+#else:
+    #print("The language selcet is not aviable")
+   
 
-a = ("Italian")
-b = ("English")
+    if LanguageSelect == list_language[0]:#LanguageOne :
+        nameA=input("Nome:  ")
+        surnameA=input("Cognome:  ")
+        ageA=input("Età:  ")
+    if len(nameA)>1 and len(surnameA) >1 and int(ageA) >= 18:
+        print("Dati accettati")
+    else:
+        print("Dati non validi")
 
-if ("Italian") == language :
- input("Nome:   ")
- input("Cognome:  ")
- input("Età:   ")
- 
+    if LanguageSelect == list_language[1]:#LanguageTwo :
+        nameB=input("Name:  ")
+        surnameB=input("Surname")
+        ageB=input("Age:  ")
+    if len(nameB)>1 and len(surnameB)>1 and int(ageB)>= 18:
+        print("Accepted data")
+    else:
+        print("Invalid data")
 
-else: ("English") == language 
-input("Name:   ")
-input("Surname:   ")
-input("Ege:   ")
-
- 
+ddsvsdv
