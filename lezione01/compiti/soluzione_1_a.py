8ffed7fc587911aa8db52fbbb6d056b763ef2941
Bob2001AC <ottaviolilliu123@gmail.com>
2022-09-15 22:51:27 +0200

    Sviluppo compito iniziale.

--- lezione01/compiti/soluzione_1_a.py
+++ lezione01/compiti/soluzione_1_a.py
@@ -1,22 +1,21 @@
-print("Benvenuti al casinò da Bob\n")
-print("Per giocare è neccessario un account\n")
-print("Inserisci i dati richiesti\n")
+#print("Benvenuti al casinò da Bob\n")
+#print("Per giocare è neccessario un account\n")
+#print("Inserisci i dati richiesti\n")
 
-nome = input("Nome: ")
-cognome = input("Cognome: ")
-eta = input("Età: ")
+#nome = input("Nome: ")
+#cognome = input("Cognome: ")
+#eta = input("Età: ")
 
-if len(nome) > 1 and len(cognome) > 1 and int(eta) >= 18:
-    print("I dati inseriti sono validi")
-    print("Il nome inserito è: " + nome)
-    print("Il cognome inserito è: " + cognome)
-    print("L'età inserita è: " + eta)
-else:
-    print("I dati non sono corretti")
+#if len(nome) > 1 and len(cognome) > 1 and int(eta) >= 18:
+    #print("I dati inseriti sono validi")
+    #print("Il nome inserito è: " + nome)
+    #print("Il cognome inserito è: " + cognome)
+    #print("L'età inserita è: " + eta)
+#else:
+    #print("I dati non sono corretti")
     
-string = nome
-nome= string.capitalize()
-print(nome)
-string = cognome
-cognome= string.capitalize()
-print(cognome)
+condizione = True
+
+while condizione:
+    print("riga uno del blocco")
+    print("riga due del blocco")
