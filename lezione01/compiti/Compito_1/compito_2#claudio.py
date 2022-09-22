f9680533eb79ae1b8c1602158932b1b3de83af0b
Bob2001AC <ottaviolilliu123@gmail.com>
2022-09-22 22:03:29 +0200

    Esercizio 2 di Claudio

---
+++ lezione01/compiti/Compito_1/compito_2#claudio.py
@@ -0,0 +1,16 @@
+nome = input("inserisci il nome: ")
+cognome = input("inserisci il cognome: ")
+eta = input("inserisci l'età: ")
+peso = input("inserisci il peso: ")
+maggiorenne = input("Sei maggiorenne?")
+altezza = input("inserisci l'altezza: ")
+
+print((type(nome)))
+print(type(cognome))
+print(type(int(eta)))
+print(type(peso))
+print(type(float(altezza)))
+
+
+
+
--- lezione01/compiti/soluzione-1-a.py
+++
@@ -1,3 +0,0 @@
-print("Benvenuti al casinò")
-print("\n")
-print("Inserire i dati richiesti")
